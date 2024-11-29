e5b3c5ca9dfab82c5fe687b666ab5b4ef5d36968
ikucuk <incikucuk98@gmail.com>
2024-11-30 02:35:12 +0300

    mapped tour list with images and show on /tour path

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const url = 'https://www.course-api.com/react-tours-project';
@@ -23,13 +24,20 @@ async function TourPage() {
 
     return <section>
         <h1 className="text-3xl mb-4">Tours</h1>
+        <div className="grid md:grid-cols-2 gap-8">
         {data.map((tour) => {
             return (
-                <Link key={tour.id} href={`/tours/${tour.id}`} className='hover:text-blue'>
-                    <h2>{tour.name}</h2>
+                <Link key={tour.id} href={`/tours/${tour.id}`} className='hover:text-blue-500'>
+                   <div className="relative h-48 mb-2"> 
+                      <Image src={tour.image} alt={tour.name} fill priority className="object-cover rounded" 
+                      sizes= '(max-width:768px) 100vw (max-width:1200px 50vw)'></Image>
+                   </div>
+                   <h2>{tour.name}</h2>
                 </Link>
             )
         })}
+        </div> 
+       
     </section>
 }
 
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+
 
 const nextConfig: NextConfig = {
   /* config options here */
